059232e9b8e4b87c50a5c944ebb4b2e4e04743b9
alvarovog <alvarovspp@outlook.com>
2024-12-16 17:48:44 -0300

    Final atividade 04

--- reactjs/Ambientesetup/reactApp/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDom from 'react-dom'
-import app from "./src/app"
-
-ReactDOM.render(<app />, document.getElementById ('root'))
--- reactjs/Ambientesetup/reactApp/src/app.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const app = () => {
-    return <h1>Olá, mundo!</h1>
-}
-
-export default app 
